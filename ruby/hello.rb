812584308e190a9d4b992bf69db8c8537bb4fb6d
fumokmm <fumo.kmm@gmail.com>
2021-03-16 00:00:27 +0900

    結果出力部分をクラス化

--- ruby/hello.rb
+++ ruby/hello.rb
@@ -1,42 +1,61 @@
-px_size = 20
-
 # 列の数 [列 → 行]
-column_nums = [
-    [5], [3, 1], [1, 1, 1], [1, 1], [5]
+col_nums = [
+    [5], [3, 1], [1, 1, 1], [1, 2], [5]
 ]
 # 行の数 [行 → 列]
 row_nums = [
-    [3, 1], [2, 2], [3, 1], [1, 2], [3, 1]
+    [3, 1], [2, 2], [3, 1], [1, 2], [5]
 ]
-
-data_row_size = 5
-data_column_size = 5
 
 result_data = [
-    ['o', 'o', 'o', 'x', 'o'],
-    ['o', 'o', 'x', 'o', 'o'],
-    ['o', 'o', 'o', 'x', 'o'],
-    ['o', 'x', 'x', 'o', 'o'],
-    ['o', 'o', 'o', 'x', 'o']
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ']
 ]
 
-column_nums_len = column_nums.collect{|it| it.size}.max
-column_nums.each{|it| while(it.size < column_nums_len) do it.unshift nil end}
-# puts column_nums_len
-row_nums_len = row_nums.collect{|it| it.size}.max
-row_nums.each{|it| while(it.size < row_nums_len) do it.push nil end}
-# puts row_nums_len
+# result_data = [
+#     ['o', 'o', 'o', 'x', 'o'],
+#     ['o', 'o', 'x', 'o', 'o'],
+#     ['o', 'o', 'o', 'x', 'o'],
+#     ['o', 'x', 'x', 'o', 'o'],
+#     ['o', 'o', 'o', 'o', 'o']
+# ]
 
-File.open('result.html', 'w') do |f|
-    f.puts <<-EOS
+# 結果出力クラス
+class ResultPrinter
+    def initialize(row_nums, col_nums, result_data)
+        @row_nums = row_nums
+        @col_nums = col_nums
+        @result_data = result_data
+        
+        @row_nums_size = @row_nums.collect{|r| r.size}.max
+        @row_nums.each{|r| while(r.size < @row_nums_size) do r.unshift nil end}
+            
+        @col_nums_size = @col_nums.collect{|c| c.size}.max
+        @col_nums.each{|c| while(c.size < @col_nums_size) do c.unshift nil end}
+
+        @result_data_row_size = @result_data.size
+        @result_data_col_size = @result_data.first.size
+
+        @px_size = 20
+    end
+
+    def normalize_nums
+    end
+
+    def exec_output
+        File.open('result.html', 'w') do |f|
+            f.puts <<-EOS
 <!doctype html>
 <html lang="ja">
 <head>
     <title>結果</title>
     <style>
         td.fixed {
-            width: #{px_size}px;
-            height: #{px_size}px;
+            width: #{@px_size}px;
+            height: #{@px_size}px;
             text-align: center;
             vertical-align: center;
         }
@@ -56,53 +75,59 @@ File.open('result.html', 'w') do |f|
     <table border="1">
 EOS
 
-(data_row_size + column_nums_len).times do |r|
-    data_row = r - column_nums_len
-    f.puts <<-EOS
-        <tr>
-    EOS
-
-    (data_column_size + row_nums_len).times do |c|
-        data_col = c - row_nums_len
-
-        # 左上
-        if r == 0 && c == 0 then
+        (@result_data_row_size + @col_nums_size).times do |r|
+            data_row = r - @col_nums_size
             f.puts <<-EOS
-                <td class="fixed" rowspan="#{column_nums_len}" colspan="#{row_nums_len}"></td>
+                <tr>
             EOS
-
-        elsif r < column_nums_len && c < row_nums_len then
-            # スキップ
         
-        # 列の数を出力
-        elsif r < column_nums_len then
+            (@result_data_col_size + @row_nums_size).times do |c|
+                data_col = c - @row_nums_size
+        
+                # 左上
+                if r == 0 && c == 0 then
+                    f.puts <<-EOS
+                    <td class="fixed" rowspan="#{@col_nums_size}" colspan="#{@row_nums_size}"></td>
+                    EOS
+        
+                elsif r < @col_nums_size && c < @row_nums_size then
+                    # スキップ
+                
+                # 列の数を出力
+                elsif r < @col_nums_size then
+                    f.puts <<-EOS
+                    <td class="fixed _">#{@col_nums[data_col][data_row + @col_nums_size]}</td>
+                    EOS
+        
+                # 行の数を出力
+                elsif c < @row_nums_size then
+                    f.puts <<-EOS
+                    <td class="fixed _">#{@row_nums[data_row][data_col + @row_nums_size]}</td>
+                    EOS
+        
+                # データを出力
+                else
+                    f.puts <<-EOS
+                    <td class="fixed #{@result_data[data_row][data_col]}"></td>
+                    EOS
+                end
+            end
+        
             f.puts <<-EOS
-                <td class="fixed _">#{column_nums[data_col][data_row + column_nums_len]}</td>
-            EOS
-
-        # 行の数を出力
-        elsif c < row_nums_len then
-            f.puts <<-EOS
-                <td class="fixed _">#{row_nums[data_row][data_col + row_nums_len]}</td>
-            EOS
-
-        # データを出力
-        else
-            f.puts <<-EOS
-                <td class="fixed #{result_data[data_row][data_col]}"></td>
+                </tr>
             EOS
         end
+        
+        f.puts <<-EOS
+            </table>
+        </body>
+        </html>
+        EOS
+        
+        end
     end
-
-    f.puts <<-EOS
-        </tr>
-    EOS
 end
 
-f.puts <<-EOS
-    </table>
-</body>
-</html>
-EOS
-
-end
+# 結果出力
+rp = ResultPrinter.new(row_nums, col_nums, result_data)
+rp.exec_output
